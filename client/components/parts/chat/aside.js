8deb4d1cad03b5cd2b3a46db42c284d59221d967
Zaslavskij <p.zaslavskij@gmail.com>
2020-09-23 16:39:17 +0700

    markup moved to separate components

--- client/components/parts/chat/aside.js
+++ client/components/parts/chat/aside.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Channel from './channel'
 
 const Aside = () => {
   return (
@@ -33,21 +34,7 @@ const Aside = () => {
             </svg>
           </div>
         </div>
-        <div className="bg-teal-dark py-1 px-4 text-white"># general</div>
-      </div>
-      <div>
-        <div className="px-4 mb-2 text-white flex justify-between items-center">
-          <div className="opacity-75">Apps</div>
-          <div>
-            <svg
-              className="fill-current h-4 w-4 opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M11 9h4v2h-4v4H9v-4H5V9h4V5h2v4zm-1 11a10 10 0 1 1 0-20 10 10 0 0 1 0 20zm0-2a8 8 0 1 0 0-16 8 8 0 0 0 0 16z" />
-            </svg>
-          </div>
-        </div>
+        <Channel />
       </div>
     </div>
   )
