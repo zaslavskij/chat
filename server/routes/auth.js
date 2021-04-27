d3d4216dae4a47cff3203cc4ec8de1e649bfa360
Zaslavskij <p.zaslavskij@gmail.com>
2021-04-27 17:36:52 +0700

    move controllers logic to it's separate path

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,40 +1,10 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-
-import config from '../config'
-import User from '../model/User.model'
+import userController from '../controllers/user'
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
-  try {
-    let user = await User.findAndValidateUser(req.body)
-    user = user.toObject()
-    const payload = { uid: user._id }
-    const token = jwt.sign(payload, config.secret, { expiresIn: '48h' })
-    user = { role: user.role, email: user.email, nikname: user.nickname }
+router.post('/', userController.login)
 
-    res.cookie('token', token, { maxAge: 1000 * 60 * 60 * 48 })
-    res.json({ status: 'ok', user, token })
-  } catch (err) {
-    res.json({ status: 'error', message: `Error occured: ${err}` })
-  }
-})
-
-router.get('/', async (req, res) => {
-  try {
-    const jwtUser = jwt.verify(req.cookies.token, config.secret)
-    let user = await User.findById(jwtUser.uid)
-
-    const payload = { uid: user._id }
-    const token = jwt.sign(payload, config.secret, { expiresIn: '48h' })
-    user = { role: user.role, email: user.email, nikname: user.nickname }
-    res.cookie('token', token, { maxAge: 1000 * 60 * 60 * 48 })
-    res.json({ status: 'ok', token, user })
-  } catch (err) {
-    console.log(err)
-    res.json({ status: 'error', err })
-  }
-})
+router.get('/', userController.auth)
 
 export default router
--- server/routes/register.js
+++ server/routes/register.js
@@ -1,23 +1,8 @@
 import express from 'express'
-import jwt from 'jsonwebtoken'
-
-import config from '../config'
-import User from '../model/User.model'
+import userController from '../controllers/user'
 
 const router = express.Router()
 
-router.post('/', async (req, res) => {
-  try {
-    const { email, password } = req.body
-    let user = new User({ email, password })
-    await user.save()
-    const token = jwt.sign({ uid: user._id }, config.secret, { expiresIn: '48h' })
-    user = { role: user.role, email: user.email, nikname: user.nickname }
-    res.cookie('token', token, { expiresIn: 1000 * 60 * 60 * 48 })
-    res.json({ status: 'ok', message: 'user was successfully registered', user, token })
-  } catch (err) {
-    res.json({ status: 'error', message: `Error occured: ${err}` })
-  }
-})
+router.post('/', userController.register)
 
 export default router
