2c16cd8236665a0f10556b50b407f7d6d50ac0e0
Zaslavskij <p.zaslavskij@gmail.com>
2021-05-11 18:13:08 +0700

    responsive classes for most of elements

--- client/components/parts/chat/aside.js
+++ client/components/parts/chat/aside.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import { FaTimes } from 'react-icons/fa'
+
 import ChannelsList from './channels-list'
 import ProfileBlock from './profile-block'
 
 const Aside = ({ user, channels, selected }) => {
   return (
-    <div className="bg-green-600 text-purple-lighter flex-none w-64 pb-6 hidden md:block select-none">
-      <div className="text-white mb-2 mt-3 px-4 flex justify-between">
+    <div className="bg-green-600 text-purple-lighter flex-none w-64 pb-6 select-none block sm:fixed sm:top-0 sm:left-0 sm:h-screen">
+      <div className="text-white mb-2 mt-3 px-4 sm:px-2 flex justify-between">
         <div className="flex-auto">
-          <h1 className="font-semibold text-xl leading-tight mb-1 truncate">Messenger</h1>
+          <div className="flex items-center leading-tight mb-1 truncate">
+            <FaTimes className="sm:flex hidden mr-2" />
+            <span className="font-semibold text-xl">Messenger</span>
+          </div>
           <ProfileBlock username={user.nickname || user.email} />
         </div>
       </div>
--- client/components/parts/chat/header.js
+++ client/components/parts/chat/header.js
@@ -4,7 +4,7 @@ import { FaBars } from 'react-icons/fa'
 const Header = ({ selected }) => {
   return (
     <div className="border-b px-3 py-2 items-center flex">
-      <FaBars className="mr-2 text-gray-500 hover:text-gray-600 text-xl" />
+      <FaBars className="mr-2 text-gray-500 hover:text-gray-600 text-xl hidden sm:flex" />
       <h3 className="text-gray-700 mb-1 font-extrabold">#{selected}</h3>
     </div>
   )
