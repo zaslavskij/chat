ad7bcae891e6253742087d88fd0bd40e5c15fed1
Zaslavskij <p.zaslavskij@gmail.com>
2021-05-16 11:19:55 +0700

    fix ux text

--- client/components/parts/chat/users-list.js
+++ client/components/parts/chat/users-list.js
@@ -6,7 +6,7 @@ const UsersList = ({ usersOnline }) => {
       <div className="text-white flex justify-between items-center">
         <span className="flex items-center">
           <span className="bg-white rounded-full block w-2 h-2 mr-2" />
-          Users online:
+          Users:
         </span>
       </div>
       {usersOnline.map((nickname) => (
