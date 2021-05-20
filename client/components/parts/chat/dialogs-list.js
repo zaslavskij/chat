7adf98f02e20d15c6ee8d631a574c95f5fde077a
Zaslavskij <p.zaslavskij@gmail.com>
2021-05-20 22:42:24 +0700

    users online

--- client/components/parts/chat/dialogs-list.js
+++ client/components/parts/chat/dialogs-list.js
@@ -4,18 +4,18 @@ const DialogsList = ({ dialogs }) => {
   return (
     <div className="py-8 mx-2  border-t border-white border-opacity-50">
       <div className="text-white flex justify-between items-center">
-        <span className="flex items-center">
-          <span className="bg-white rounded-full block w-2 h-2 mr-2" />
-          Users:
-        </span>
+        <span className="flex items-center ml-4">Users:</span>
       </div>
       {Object.keys(dialogs).map((title) => (
         <div
-          className={`px-4  ${
+          className={`px-4 flex items-center ${
             dialogs[title].online ? 'opacity-100 text-medium' : 'opacity-75'
           }  text-white`}
           key={title}
         >
+          {dialogs[title].online && (
+            <span className="bg-white rounded-full block w-2 h-2 mr-1 mt-1" />
+          )}
           {title}
         </div>
       ))}
