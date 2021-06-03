06d226014346152f7d77a81edf519b265f881792
Zaslavskij <p.zaslavskij@gmail.com>
2021-06-03 19:49:40 +0700

    fix protocol for image

--- client/components/parts/chat/message.js
+++ client/components/parts/chat/message.js
@@ -5,7 +5,7 @@ import ReactMarkdown from 'react-markdown'
 const Message = ({ date, time, nickname, message }) => {
   return (
     <div className="flex items-start mb-4 text-sm">
-      <img src="http://placehold.it/300" className="w-12 h-12 rounded mr-3" alt="" />
+      <img src="//placehold.it/300" className="w-12 h-12 rounded mr-3" alt="" />
       <div className="flex-1 overflow-hidden">
         <div className="flex flex-col">
           <span className="font-bold">{nickname}</span>
